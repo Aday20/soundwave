b0df82fb8e06de6cf4377f70069e0d14bbe2271e
Abdullahi Yusuf <120734150+Aday20@users.noreply.github.com>
2022-12-21 22:13:37 -0800

    done with the navigation

---
+++ js/nav.js
@@ -0,0 +1,13 @@
+const menu_btn = document.getElementById('hamburger');
+const mobile_menu = document.getElementById('navigation');
+document.onclick = function(e){
+    if(e.target.id !== 'navigation' && e.target.id !== 'hamburger')
+    {
+        menu_btn.classList.remove('is-active');
+        mobile_menu.classList.remove('is-active');
+    }
+}
+menu_btn.onclick = function(){
+    menu_btn.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-active');
+};
